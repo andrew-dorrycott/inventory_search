af7d5888447ddea754eb59dbf3d830aab44176c5
andrew-dorrycott <andrew.dorrycott@gmail.com>
2019-08-01 20:00:39 -0500

    Added Missing Exit on Failure & Added Base Elements for SQLAlchemy

--- process_csv.py
+++ process_csv.py
@@ -2,6 +2,24 @@
 import csv
 import sys
 
+# Third party imports
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import yaml
+
+
+def load_config():
+    with open("config.yaml", "r") as _file:
+        return yaml.load(_file, Loader=yaml.FullLoader)
+
+
+def load_db(config):
+    engine = sqlalchemy.create_engine(
+        config["postgresql"]["sqlalchemy_uri"].format(**config["postgresql"])
+    )
+    Session = sessionmaker(bind=engine)
+    return Session()
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -10,6 +28,9 @@ if __name__ == "__main__":
                 sys.argv[0]
             )
         )
+        sys.exit()
+
+    session = load_db(load_config())
 
     with open(sys.argv[1], "r") as _file:
         reader = csv.DictReader(_file)
